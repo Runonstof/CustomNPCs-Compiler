37d1b6b271603f9ee42aba1a4ed7cef6d750b010
Runonstof <lichtronanstof@hotmail.com>
2020-08-07 22:39:00 +0200

    Update js.php

--- config/syntax/js.php
+++ config/syntax/js.php
@@ -1,13 +1,22 @@
 <?php
 
 return [
-    'import' =>  '/import\s+(?:"|\')([\S\-\w.\\\*\/]+\.[a-zA-Z\\*]+)(?:"|\')(?:\s+with\s+({[\s\S]*?}))?(?:\s+as\s+([\w]+))?\s*;/',
-    'defineBlock' => '/@block ([\w]+)(?:\;)?([\w\W\r\n]+?)@endblock(?:\;)?/',
+    // 'import' =>  '/import\s+(?:"|\')([\S\-\w.\\\*\/]+\.[a-zA-Z\\*]+)(?:"|\')(?:\s+with\s+({[\s\S]*?}))?(?:\s+as\s+([\w]+))?\s*;/',
+    'import' =>  '/^(?!\/\/\s*)import\s+(?:{([\s\S]*?)}\s+from\s+)?(?:(?:"|\')([\S\-\w.\\\*\/]+\.[a-zA-Z\\*]+)(?:"|\')|([\w\S.]+))(?:\s+with\s+({[\s\S]*?}))?(?:\s+as\s+([\w]+))?\s*;/m',
+    'defineBlock' => '/@block\s+([\w]+)(?:\;)?([\w\W\r\n]+?)@endblock(?:\;)?/',
     'getBlock' => '/@yield ([\w\-]+);/',
     'forLoopTweak' => '/for\s*\(\s*(?:var\s+)?(\w+)\s+in\s+([\w.()\[\]\"\',+\-\/*\s]+)\s+as\s+(\w+)\s*\)\s*{/',
     'annotations' => '/@([\w]+)(?:\(([\s\S]+)\))?;/',
+    'functionTweak' => '/function(?:\s+([\w]+))?\s*\(([\w\S\s]*?)\)[\s]*{/',
+    'functionTweakArgs' => '/([\w]+)(?:[\s]*=[\s]*([\w\S]+)(?:\s*,|\s*$))?/',
 
     //=== Decompiler regex
     'decompMCP' => '/import\s+(method|field)\s+([\w\.]+)\s+as\s+([\w]+)\s*;\s*/',
     'decompMCPGet' => '/\.@([\w]+)/',
+
+    //==Gui ID Reserver
+    'guiIdFunc' => '/@(id\([\w\s\W]+?\))\$/',
+
+    //JSModules
+    'defineModule' => '/@module\s+(?:function\s+)?([\w]+)(?:\(([\w\W]*?)\)(?:\s*:\s*[\w\S\s]+?)?)?(?:\s+requires\s+{([\s\S]*?)})?\;([\w\W\r\n]+?)@endmodule(?:\s+\1+)?;/'
 ];
